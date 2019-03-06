b53906d111211eead754d331766f14e2ec4a247f
waka <wa_morita@route-zero.com>
2019-03-06 20:32:36 +0900

    HP参考課題

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace MeteorGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pTitle = new System.Windows.Forms.PictureBox();
+            this.pPlayer = new System.Windows.Forms.PictureBox();
+            this.pMsg = new System.Windows.Forms.PictureBox();
+            this.pGameover = new System.Windows.Forms.PictureBox();
+            this.pExp = new System.Windows.Forms.PictureBox();
+            this.pBase = new System.Windows.Forms.PictureBox();
+            this.pMeteor = new System.Windows.Forms.PictureBox();
+            this.pBG = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pTitle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pMsg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pGameover)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pExp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBase)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pMeteor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pTitle
+            // 
+            this.pTitle.Image = global::MeteorGame.Properties.Resources.p_title;
+            this.pTitle.Location = new System.Drawing.Point(68, 157);
+            this.pTitle.Name = "pTitle";
+            this.pTitle.Size = new System.Drawing.Size(100, 50);
+            this.pTitle.TabIndex = 8;
+            this.pTitle.TabStop = false;
+            // 
+            // pPlayer
+            // 
+            this.pPlayer.Image = global::MeteorGame.Properties.Resources.p_player;
+            this.pPlayer.Location = new System.Drawing.Point(86, 32);
+            this.pPlayer.Name = "pPlayer";
+            this.pPlayer.Size = new System.Drawing.Size(100, 50);
+            this.pPlayer.TabIndex = 7;
+            this.pPlayer.TabStop = false;
+            // 
+            // pMsg
+            // 
+            this.pMsg.Image = global::MeteorGame.Properties.Resources.p_msg;
+            this.pMsg.Location = new System.Drawing.Point(258, 200);
+            this.pMsg.Name = "pMsg";
+            this.pMsg.Size = new System.Drawing.Size(100, 50);
+            this.pMsg.TabIndex = 6;
+            this.pMsg.TabStop = false;
+            // 
+            // pGameover
+            // 
+            this.pGameover.Image = global::MeteorGame.Properties.Resources.p_gameover;
+            this.pGameover.Location = new System.Drawing.Point(128, 176);
+            this.pGameover.Name = "pGameover";
+            this.pGameover.Size = new System.Drawing.Size(100, 50);
+            this.pGameover.TabIndex = 4;
+            this.pGameover.TabStop = false;
+            // 
+            // pExp
+            // 
+            this.pExp.Image = global::MeteorGame.Properties.Resources.p_explosion;
+            this.pExp.Location = new System.Drawing.Point(175, 88);
+            this.pExp.Name = "pExp";
+            this.pExp.Size = new System.Drawing.Size(100, 50);
+            this.pExp.TabIndex = 3;
+            this.pExp.TabStop = false;
+            // 
+            // pBase
+            // 
+            this.pBase.Location = new System.Drawing.Point(-31, -9);
+            this.pBase.Name = "pBase";
+            this.pBase.Size = new System.Drawing.Size(548, 353);
+            this.pBase.TabIndex = 2;
+            this.pBase.TabStop = false;
+            this.pBase.Click += new System.EventHandler(this.pBase_Click);
+            // 
+            // pMeteor
+            // 
+            this.pMeteor.Image = global::MeteorGame.Properties.Resources.p_meteor;
+            this.pMeteor.Location = new System.Drawing.Point(323, 72);
+            this.pMeteor.Name = "pMeteor";
+            this.pMeteor.Size = new System.Drawing.Size(71, 73);
+            this.pMeteor.TabIndex = 1;
+            this.pMeteor.TabStop = false;
+            // 
+            // pBG
+            // 
+            this.pBG.Image = global::MeteorGame.Properties.Resources.p_bg;
+            this.pBG.Location = new System.Drawing.Point(-70, -52);
+            this.pBG.Name = "pBG";
+            this.pBG.Size = new System.Drawing.Size(345, 222);
+            this.pBG.TabIndex = 0;
+            this.pBG.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 281);
+            this.Controls.Add(this.pTitle);
+            this.Controls.Add(this.pPlayer);
+            this.Controls.Add(this.pMsg);
+            this.Controls.Add(this.pGameover);
+            this.Controls.Add(this.pExp);
+            this.Controls.Add(this.pBase);
+            this.Controls.Add(this.pMeteor);
+            this.Controls.Add(this.pBG);
+            this.Name = "Form1";
+            this.Text = "MeteorGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pTitle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pMsg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pGameover)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pExp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBase)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pMeteor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBG)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBG;
+        private System.Windows.Forms.PictureBox pMeteor;
+        private System.Windows.Forms.PictureBox pBase;
+        private System.Windows.Forms.PictureBox pExp;
+        private System.Windows.Forms.PictureBox pGameover;
+        private System.Windows.Forms.PictureBox pMsg;
+        private System.Windows.Forms.PictureBox pPlayer;
+        private System.Windows.Forms.PictureBox pTitle;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MeteorGame
+{
+    public partial class Form1 : Form
+    {
+        static Bitmap canvas = new Bitmap(480, 320);
+        Graphics gg = Graphics.FromImage(canvas);
+        int PW, PH;
+        Point Cpos;//カーソル座標
+        int[] enX = new int[10];
+        int[] enY = new int[10];
+        Random rand = new Random();
+        int RR;
+        Boolean hitFlg;//true:当たった
+        int ecnt, ex, ey;//爆発演出用
+        long msgcnt;//メッセージ用カウンタ
+        Boolean titleFlg;//true：タイトル表示中
+        long score;
+        Font myFont = new Font("Arial", 16);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pMeteor.Hide();
+            pPlayer.Hide();
+            pBG.Hide();
+            pExp.Hide();
+            pGameover.Hide();
+            pMsg.Hide();
+            pTitle.Hide();
+
+            pBase.Image = canvas;
+
+            initGame();//初期処理
+        }
+
+        private void initGame()
+        {
+            PW = 41;//自機の幅
+            PH = 51;//自機の高さ
+            RR = 70 / 2;//隕石の半径
+            for(int i=0;i<10;i++)
+            {
+                enX[i] = rand.Next(1, 450);//隕石の初期配置座標
+                enY[i] = rand.Next(1, 900) - 1000;
+            }
+            hitFlg = false;
+            ecnt = 40;//爆発の初めの処理で位置を変更する
+            msgcnt = 0;
+            titleFlg = true;
+            score = 0;
+        }
+
+        private void pBase_Click(object sender, EventArgs e)
+        {
+            if(titleFlg)
+            {
+                if(msgcnt>20)
+                {
+                    msgcnt = 0;
+                    titleFlg = false;
+                }
+                return;
+            }
+            if(msgcnt>80)
+            {
+                initGame();
+            }
+        }
+
+        private void dispTitle()
+        {
+            msgcnt++;
+            //タイトル表示中の描画は、すべてここで行う
+            gg.DrawImage(pBG.Image, new Rectangle(0, 0, 480, 320));
+            gg.DrawImage(pTitle.Image, new Rectangle(70, 80, 350, 60));
+            if(msgcnt%60>20)
+            {
+                gg.DrawImage(pMsg.Image, new Rectangle(110, 190, 271, 26));
+            }
+            pBase.Image = canvas;
+        }
+
+        private void playerExplosion()
+        {
+            ecnt+= 4;
+            if(ecnt>40)
+            {
+                ecnt = 8;
+                ex = Cpos.X + rand.Next(40);//爆発の位置を変更
+                ey = 220 + rand.Next(50);
+            }
+            //爆発演出中の描画は、すべてここで行う
+            gg.DrawImage(pBG.Image, new Rectangle(0, 0, 480, 320));
+            for(int i=0;i<10;i++)
+            {
+                gg.DrawImage(pMeteor.Image, new Rectangle(enX[i], enY[i], RR * 2, RR * 2));
+            }
+            gg.DrawImage(pPlayer.Image, new Rectangle(Cpos.X, 220, PW, PH));
+            gg.DrawImage(pExp.Image, new Rectangle(ex - ecnt / 2, ey - ecnt / 2, ecnt, ecnt));
+            msgcnt++;
+            if(msgcnt>60)
+            {
+                gg.DrawImage(pGameover.Image, new Rectangle(70, 80, 350, 60));
+                if(msgcnt%60>20)
+                {
+                    gg.DrawImage(pMsg.Image, new Rectangle(110, 190, 271, 26));
+                }
+            }
+            gg.DrawString("SCORE:" + score.ToString(), myFont, Brushes.White, 10, 10);
+            pBase.Image = canvas;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(titleFlg)
+            {
+                dispTitle();
+                return;
+            }
+            if(hitFlg)
+            {
+                playerExplosion();
+                return;
+            }
+            gg.DrawImage(pBG.Image, new Rectangle(0, 0, 480, 320));
+
+            //隕石の移動
+            for(int i=0;i<10;i++)
+            {
+                enY[i] += 5;
+                gg.DrawImage(pMeteor.Image, new Rectangle(enX[i], enY[i], RR * 2, RR* 2));
+                if(enY[i]>pBase.Height)//画面外へ出たら上に戻す
+                {
+                    enX[i] = rand.Next(1, 450);
+                    enY[i] = rand.Next(1, 300) - 400;
+                }
+            }
+            Cpos = PointToClient(Cursor.Position);
+            if(Cpos.X<0)
+            {
+                Cpos.X = 0;
+            }
+            if(Cpos.X>Width-PW)
+            {
+                Cpos.X = Width - PW;
+            }
+            gg.DrawImage(pPlayer.Image, new Rectangle(Cpos.X, 220, PW, PH));
+
+            score++;
+            gg.DrawString("SCORE:" + score.ToString(), myFont, Brushes.White, 10, 10);
+            pBase.Image = canvas;
+            hitCheck();
+        }
+
+        private void hitCheck()
+        {
+            int pcx = Cpos.X + (PW / 2);//自機の中心座標
+            int pcy = 220 + (PH / 2);
+            int ecx, ecy, dis;//自機と隕石の距離計算用
+
+            
+            for(int i=0;i<10;i++)
+            {
+                ecx = enX[i] + RR;
+                ecy = enY[i] + RR;
+                dis = (ecx - pcx) * (ecx - pcx) + (ecy - pcy) * (ecy - pcy);
+                if(dis<RR*RR)
+                {
+                    hitFlg=true;
+                    break;
+                }
+            }
+        }
+    }
+}
